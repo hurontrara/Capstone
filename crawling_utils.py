db648e9188188b2ef27fee854fc705a7ce72e8bd
최선빈 <105468810+hurontrara@users.noreply.github.com>
2023-06-02 15:09:45 +0900

    Add files via upload

--- crawling_utils.py
+++ crawling_utils.py
@@ -13,10 +13,11 @@ def add_check(studentNum, password): # 아디 / 비번 받아서 로그인 가�
     chrome_options.add_argument('--disable-dev-shm-usage')
 
     try:   
-        driver = webdriver.Chrome('/home/ubuntu/Capstone/chromedriver', chrome_options=chrome_options)  # 경로 수정 필요
-    except:
         driver = webdriver.Chrome('C:/Users/Study/Desktop/Projects/Capstone/source/chromedriver.exe', chrome_options=chrome_options)
-    
+
+    except:
+        driver = webdriver.Chrome('/home/ubuntu/Capstone/chromedriver', chrome_options=chrome_options)  # 경로 수정 필요
+
     driver.get('https://wis.hufs.ac.kr/src08/jsp/index.jsp')  # 종정시 로그인 페이지
     driver.find_element_by_xpath('/html/body/div/form[3]/div[2]/div/div[2]/div/input[1]').send_keys(f'{studentNum}')
     driver.find_element_by_xpath('/html/body/div/form[3]/div[2]/div/div[2]/div/input[2]').send_keys(f'{password}')
@@ -58,9 +59,9 @@ def entranceSystem(id, password):   # 아디 빌려서 체크
     chrome_options.add_argument('--disable-dev-shm-usage')
 
     try:   
-        driver = webdriver.Chrome('/home/ubuntu/Capstone/chromedriver', chrome_options=chrome_options)  # 경로 수정 필요
-    except:
         driver = webdriver.Chrome('C:/Users/Study/Desktop/Projects/Capstone/source/chromedriver.exe', chrome_options=chrome_options)
+    except:
+        driver = webdriver.Chrome('/home/ubuntu/Capstone/chromedriver', chrome_options=chrome_options)  # 경로 수정 필요
 
     driver.get('https://wis.hufs.ac.kr/src08/jsp/index.jsp')  # 종정시 로그인 페이지
     driver.find_element_by_xpath('/html/body/div/form[3]/div[2]/div/div[2]/div/input[1]').send_keys(f'{id}')
